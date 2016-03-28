97616ad3196f9c72e8355ee3d236ccec111002cb
steph-harris <steph.harris21@gmail.com>
2016-03-28 16:02:26 -0400

    added routes folder and some basic controllers

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var PORT = 8080
 app.use(logger('dev'));
 app.use(express.static(__dirname + "/public"));
 
+app.get('*', function(req, res){
+  res.sendFile(process.cwd() + '/index.html');
+});
+
+require('./routes')(app);
+
 app.listen(PORT, function(){
   console.log("listening on PORT:" + PORT);
 });
+
+
---
+++ app_server/controllers/usrController.js
@@ -0,0 +1,5 @@
+exports.test = function(req, res){
+  console.log("this worked!");
+}
+
+
