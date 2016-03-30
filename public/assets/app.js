8f84cd14d98312eb4100eeed2f9f90e95290ca84
eliezerwohl <eliezerwohl@gmail.com>
2016-03-30 17:53:16 -0400

    changed cdn, changed to ui router from ng router

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,15 +1,35 @@
-angular.module("ClassApp", ['ngRoute'])
-  .config(function($routeProvider, $locationProvider){
-    $routeProvider
-      .when('/home', {
-        templateUrl:"/views/home.html"
-      })
-      .when('/users', {
-        templateUrl:"/views/users.html"
-      });
+var classApp = angular.module('classApp', ['ui.bootstrap','ui.router']);
+  // .config(function($routeProvider, $locationProvider){
+  //   $routeProvider
+  //     .when('/home', {
+  //       templateUrl:"/views/home.html"
+  //     })
+  //     .when('/users', {
+  //       templateUrl:"/views/users.html"
+  //     });
 
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
+  //   $locationProvider.html5Mode({
+  //     enabled: true,
+  //     requireBase: false
+  //   });
+  // });
+
+
+classApp.config(function($stateProvider, $urlRouterProvider) {
+$urlRouterProvider.otherwise('/home')
+$stateProvider
+// HOME STATES AND NESTED VIEWS ========================================
+  .state('home', {
+    url: '/home',
+    templateUrl: '/views/home.html'
+  })
+    .state('users', {
+    url: '/users',
+    templateUrl: '/views/users.html'
+  })
+    .state('register', {
+    url: '/register',
+    templateUrl: '/views/register.html'
+  })
+
   });
