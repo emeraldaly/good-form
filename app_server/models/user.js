0ca00a942baab2b5779cb0961cbfba92b45de4c1
evan bates <evan.bates@rutgers.edu>
2016-03-31 10:39:16 -0400

    jwt

--- app_server/models/user.js
+++ app_server/models/user.js
@@ -1,5 +1,7 @@
 	var mongoose = require("mongoose");
-  
+  var crypto = require("crypto");
+  var jwt = require("jsonwebtoken");
+
   var Schema = mongoose.Schema;
 
   var UserSchema = new Schema({
@@ -23,6 +25,31 @@
         ref: 'Class'
       }]
   });
+
+  UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  };
+
+  UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+
+  };
+
+  UserSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    return jwt.sign({
+      _id : this._id,
+      email: this.email,
+      name: this.name,
+      exp: parseInt(expiry.getTime() / 1000),
+    }, process.env.JWT_SECRET);
+};
+  
+  
 
 var User = mongoose.model('User', UserSchema);
 
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+require("dotenv").load();
 var express = require("express");
 var app = express();
+var cookieParser = require("cookie-parser");
 var bodyParser = require ("body-parser");
 var logger= require('morgan');
 var PORT = process.env.PORT || 8080;
 var method = require("method-override");
+var passport = require("passport");
+
+require("./app_server/models/db");
+require('./routes')(app);
+
 app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
  	extended: false
 }));
 
+app.use(passport.initialize());
 // app.get('*', function(req, res){
 //   res.sendFile(process.cwd() + '/index.html');
 // });
 
-require("dotenv").config();
-require("./app_server/models/db");
-require('./routes')(app);
 
 app.listen(PORT, function(){
   console.log("listening on PORT:" + PORT);
