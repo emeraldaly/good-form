d958537575a2b05b17bd80a9763984105da7a8be
evan bates <evan.bates@rutgers.edu>
2016-04-01 11:32:00 -0400

    logo

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-require("dotenv").load();
 var express = require("express");
 var app = express();
-var cookieParser = require("cookie-parser");
 var bodyParser = require ("body-parser");
 var logger= require('morgan');
 var PORT = process.env.PORT || 8080;
 var method = require("method-override");
 var passport = require("passport");
-
-require("./app_server/models/db");
-require('./routes')(app);
-
 app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
  	extended: false
 }));
 
-app.use(passport.initialize());
+
+require("./app_server/models/db");
+require('./routes')(app);
+
+// app.use(passport.initialize());
 // app.get('*', function(req, res){
 //   res.sendFile(process.cwd() + '/index.html');
 // });
