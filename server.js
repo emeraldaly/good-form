2ba5d2289e8f7776c323c4f688e77ea8d36ef11a
Alyson <emerald.aly@gmail.com>
2016-04-01 16:28:08 -0400

    added some html files and updated readme with hreoku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require ("body-parser");
-var logger= require('morgan');
+var logger = require('morgan');
 var PORT = process.env.PORT || 8080;
 var method = require("method-override");
 app.use(express.static('public'));
