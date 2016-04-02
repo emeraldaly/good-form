a84c27850af6d165da6502160ef773d2b3de6bfa
eliezerwohl <eliezerwohl@gmail.com>
2016-04-02 12:42:19 -0400

    added a http angular request

--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,6 @@ module.exports = function (app) {
   //User Controls
   app.post('/adduser', usrCtrl.addUser);
   app.post('/newUser', usrCtrl.newUser);
-  app.get('*', usrCtrl.defRoute);
 
   //Org Controls
   app.get('/allOrgs', orgCtrl.showAllOrgs);
