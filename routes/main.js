c13ea4c64c6c7ee893836d65a8102fe95ee20488
evan bates <evan.bates@rutgers.edu>
2016-04-04 12:43:36 -0400

    highcharts stub

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
-var usrCtrl = require('../app_server/controllers/usrController');
 var hwCtrl = require("../app_server/controllers/hwController");
 var classCtrl = require("../app_server/controllers/classController");
 var orgCtrl = require("../app_server/controllers/orgController");
+var usrCtrl = require("../app_server/controllers/usrController");
 var passport = require('passport');
 var LocalStrategy   = require('passport-local').Strategy;
 var bcrypt = require('bcryptjs');
@@ -12,11 +12,11 @@ var User = require('../app_server/models/user');
 module.exports = function (app) {
 
   app.use(require('express-session')(
-    {
-      secret: 'a_rabbit_named_bruno',
-      resave: true,
-      saveUninitialized: true,
-      cookie: { secure: false, maxAge: ( 4 * 60 * 60 * 1000 )
+  {
+    secret: 'a_rabbit_named_bruno',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: ( 4 * 60 * 60 * 1000 )
     }
   }));
 
@@ -26,11 +26,11 @@ module.exports = function (app) {
   //User Controls
   app.post('/adduser', usrCtrl.addUser);
   app.post('/newUser', usrCtrl.newUser);
-app.post('/login',
-  passport.authenticate('login', { successRedirect: '/?msg=loginsuccess',
-                                   failureRedirect: '/?msg=loginfail'
-                                  })
-);
+  app.post('/login',
+    passport.authenticate('login', { successRedirect: '/?msg=loginsuccess',
+     failureRedirect: '/?msg=loginfail'
+   })
+    );
 
   //Org Controls
   app.get('/allOrgs', orgCtrl.showAllOrgs);
@@ -49,23 +49,23 @@ app.post('/login',
   app.get('*', usrCtrl.defRoute);
 
 //passport
-  passport.serializeUser(function(user, done) {
-    console.log('serialize')
- 
-    done(null, user);
-  });
+passport.serializeUser(function(user, done) {
+  console.log('serialize')
 
-  passport.deserializeUser(function(user, done) {
-    console.log('deserialize')
-   
-    done(null, user);
-  });
- 
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log('deserialize')
+
+  done(null, user);
+});
+
 
 passport.use('login', new LocalStrategy({
-    passReqToCallback : true
-  },
-  function(req, username, password, done) { 
+  passReqToCallback : true
+},
+function(req, username, password, done) { 
     // check in mongo if a user with username exists or not
     console.log("username");
     User.findOne({ 'username' :  username }, 
@@ -77,33 +77,31 @@ passport.use('login', new LocalStrategy({
         if (!user){
           console.log('User Not Found with username '+username);
           return done(null, false, 
-                console.log('message', 'User Not found.'));                 
+            console.log('message', 'User Not found.'));                 
         }
         // User exists but wrong password, log the error 
         if (!isValidPassword(user, password)){
-        
+
           return done(null, false, 
-              console.log('message', 'Invalid Password'));
+            console.log('message', 'Invalid Password'));
         }
         // User and password both match, return user from 
         // done method which will be treated like success
-        debugger
         req.session.user = user;
         req.session.organization = user._doc._organization
         console.log(user)
         console.log(req.session.user)
         return done(null, user);
       }
-    );
-}));
+      );
+  }));
 
-    var isValidPassword = function(user, password){
-    
-      
-        return bcrypt.compareSync(password, user.password);
-    }
-    
+var isValidPassword = function(user, password){
+
+
+  return bcrypt.compareSync(password, user.password);
+}
+
 
 
 }
-    
