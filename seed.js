a1c34de435cab7c2dc0b9ba9601d58baa0cfdaad
Alyson <emerald.aly@gmail.com>
2016-04-05 13:04:40 -0400

    added seed file structure

---
+++ seed.js
@@ -0,0 +1,150 @@
+var organization1 = new Organization ({
+  name: 'Rutgers Coding Bootcamp',
+  address: 'Somerset & Jersey City',
+  website: 'https://codingbootcamp.rutgers.edu/',
+  class: [],
+  user: []
+
+});
+
+
+var class1 = new Class ({
+  name: '10-26-NB',
+  datetime: '10/26/2015-04/30/2016' ,
+  organization: 'Rutgers Coding Bootcamp',
+  student: [],
+  teacher: [],
+  ta: []
+});
+
+var class2 = new Class ({
+  name: '10-27-NB',
+  datetime: '10/27/2015-04/30/2016' ,
+  organization: 'Rutgers Coding Bootcamp',
+  student: [],
+  teacher: [],
+  ta: []
+});
+
+
+var guest = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+});
+
+var user1 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var user2 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var user3 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var user4 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var user5 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var ta1 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+var teacher1 = new User ({
+_class:
+_organization:
+linkedin:
+github:
+admin:
+firstname:
+lastname:
+email:
+username:
+password:
+_assignment:
+
+});
+
+
+
