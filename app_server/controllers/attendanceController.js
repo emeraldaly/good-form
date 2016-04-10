454dce538d0dec3780ca73ebbbe6cdf22ac45ffc
eliezerwohl <eliezerwohl@gmail.com>
2016-04-10 12:55:20 -0400

    creates new attend based on class, pushes student into array, default value of them not being there

--- app_server/controllers/attendanceController.js
+++ app_server/controllers/attendanceController.js
@@ -3,5 +3,51 @@ var User = require("../models/user");
 var Attendance = require("../models/attendance");
 
 exports.newAttendance = function(req,res){
-console.log("hit it")
+
+    
+Class.find({
+		_id: req.session.editClassId
+	})
+	.exec(function(err, docs) {
+		console.log(docs)
+		if (err) {
+			console.log(err);
+			res.send(err);
+		} else {
+
+			//        docs[0]._doc.role
+			var studentArray = docs[0]._doc.role
+
+			var newAttend = new Attendance({
+				_class: req.session.editClassId,
+			})
+			newAttend.save(function(err, doc) {
+				if (err) {
+					console.log(err);
+				} else {
+					debugger
+					console.log(studentArray)
+					for (var i = 0; i < studentArray.length; i++) {
+
+						Attendance.update({
+							_id: doc._doc._id
+						}, {
+							$push: {
+								students: {
+									_user: studentArray[i]._doc._user
+								}
+							}
+						}, {
+							safe: true,
+							upsert: true
+						}, function(err, model) {
+							debugger
+							console.log("it worked?")
+						})
+					}
+				}
+			})
+		}
+	})
+
 }
