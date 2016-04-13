352f83cd19b9a91673a9eb41c80902f68ed11d04
eliezerwohl <eliezerwohl@gmail.com>
2016-04-13 17:04:03 -0400

    fixed merge conflict

--- app_server/controllers/lectureController.js
+++ app_server/controllers/lectureController.js
@@ -6,6 +6,7 @@ console.log(req.body.github)
 var newLec = new Lecture({"github": req.body.github, "videoLink":req.body.videoLink,  
 	"info":req.body.info, 
 	"poster":req.session.user._id,
+	"date":req.body.date,
 	"_class":req.session.editClassId});
   newLec.save(function (err, doc) {
     if (err) {
