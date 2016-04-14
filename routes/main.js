b5a5832e52f472e84c39279b853683ddc9cbd1ef
evan bates <evan.bates@rutgers.edu>
2016-04-13 22:29:46 -0400

    mc 2

--- routes/main.js
+++ routes/main.js
@@ -50,7 +50,6 @@ module.exports = function (app) {
   // app.get("/viewLecture", lecture.viewLecture)
   //for getting the lectures for the logged in user
   app.get("/myLecture", lecture.myLecture)
->>>>>>> 6d56998ab8bf64fee5156bd6e1131b846ba76e30
   //HW Controls
   app.post('/submitHw', hwCtrl.submitHw);
   app.post('/createHomework', hwCtrl.createHw);
