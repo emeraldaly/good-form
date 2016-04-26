046da1aacb3f82b6754d636c65fe201e0a297677
evan bates <evan.bates@rutgers.edu>
2016-04-26 17:15:03 -0400

    little fixes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var favicon = require('serve-favicon');
 var method = require("method-override");
 
 app.use(logger('dev'));
@@ -16,13 +15,8 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
-app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
-
 require("./app_server/models/db");
-
 require('./routes')(app);
-
-
 app.listen(PORT, function(){
 	console.log("listening on PORT:" + PORT);
 });
